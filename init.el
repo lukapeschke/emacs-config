e60fc6ca72cef95a910eda7a09f98cc51398c040
Luka Peschke <mail@lukapeschke.com>
2020-04-03 11:56:01 +0200

    Added rainbow-mode
    Signed-off-by: Luka Peschke <mail@lukapeschke.com>

--- init.el
+++ init.el
@@ -28,8 +28,8 @@
   '(blacken company company-lsp dockerfile-mode dockerfile-mode drag-stuff
             elixir-mode flycheck gnu-elpa-keyring-update go-mode groovy-mode
             highlight-indentation jinja2-mode lsp-mode lsp-ui protobuf-mode
-            rainbow-delimiters rust-mode sbt-mode scala-mode terraform-mode
-            tangotango-theme use-package whitespace yaml-mode)
+            rainbow-delimiters rainbow-mode rust-mode sbt-mode scala-mode
+            terraform-mode tangotango-theme use-package whitespace yaml-mode)
   "A list of packages to ensure are installed at launch.")
 
 (defun my-packages-installed-p ()
@@ -226,7 +226,7 @@
  '(groovy-indent-offset 2)
  '(package-selected-packages
    (quote
-    (sbt-mode scala-mode blacken groovy-mode protobuf-mode elixir-mode yaml-mode use-package tangotango-theme terraform-mode rust-mode rainbow-delimiters lsp-ui jinja2-mode highlight-indentation gnu-elpa-keyring-update go-mode flycheck drag-stuff dockerfile-mode company-lsp company))))
+    (rainbow-mode sbt-mode scala-mode blacken groovy-mode protobuf-mode elixir-mode yaml-mode use-package tangotango-theme terraform-mode rust-mode rainbow-delimiters lsp-ui jinja2-mode highlight-indentation gnu-elpa-keyring-update go-mode flycheck drag-stuff dockerfile-mode company-lsp company))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
